ae1f7ee49b4856678dd897ab4d80e482d137cb56
fullyalive <wogud1202@gmail.com>
2019-04-22 19:23:36 +0900

    xlsx 패키지로 엑셀 파싱하기

--- lecture/index.js
+++ lecture/index.js
@@ -1,8 +1,27 @@
-const parse = require("csv-parse/lib/sync");
-const fs = require("fs");
+// csv
 
-const csv = fs.readFileSync("csv/data.csv");
-const records = parse(csv.toString("utf-8"));
+// const parse = require("csv-parse/lib/sync");
+// const fs = require("fs");
+
+// const csv = fs.readFileSync("csv/data.csv");
+// const records = parse(csv.toString("utf-8"));
+// records.forEach((r, i) => {
+//   console.log(i, r);
+// });
+
+// xlsx
+
+const xlsx = require("xlsx");
+const workbook = xlsx.readFile("xlsx/data.xlsx");
+
+const ws = workbook.Sheets.영화목록;
+const records = xlsx.utils.sheet_to_json(ws);
+// console.log(records);
+
 records.forEach((r, i) => {
-  console.log(i, r);
+  console.log(i, r.제목, r.링크);
 });
+
+// for (const [i, r] of records.entries()) {
+//   console.log(i, r.제목, r.링크);
+// }
