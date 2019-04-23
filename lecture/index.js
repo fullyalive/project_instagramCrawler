d70d31b5667c86226fdac4ae0fb5151d90eabd8d
fullyalive <wogud1202@gmail.com>
2019-04-23 22:49:44 +0900

    for of 문을 이용해 순차적으로 크롤링하기

--- lecture/index.js
+++ lecture/index.js
@@ -39,17 +39,26 @@ for (const [i, r] of records.entries()) {
 }
 
 const crawler = async () => {
-  await Promise.all(
-    records.map(async r => {
-      const response = await axios.get(r.링크);
-      if (response.status === 200) {
-        const html = response.data;
-        const $ = cheerio.load(html); // cheerio와 $ 를 이용해서 html tag를 가져올 수 있음
-        const score = $('.score.score_left .star_score').text();
-        console.log(r.제목, '평점 :', score.trim());
-      }
-    })
-  );
+  // await Promise.all( // Promise.all은 동시에 진행되지만 요청순서대로 값을 받는 것이 보장되지 않는다.
+  //   records.map(async r => {
+  //     const response = await axios.get(r.링크);
+  //     if (response.status === 200) {
+  //       const html = response.data;
+  //       const $ = cheerio.load(html); // cheerio와 $ 를 이용해서 html tag를 가져올 수 있음
+  //       const score = $('.score.score_left .star_score').text();
+  //       console.log(r.제목, '평점 :', score.trim());
+  //     }
+  //   })
+  // );
+  for (const [i, r] of records.entries()) { // for of 문은 await와 같이 쓰면 순서가 보장된다.
+    const response = await axios.get(r.링크);
+    if (response.status === 200) {
+      const html = response.data;
+      const $ = cheerio.load(html); // cheerio와 $ 를 이용해서 html tag를 가져올 수 있음
+      const score = $(".score.score_left .star_score").text();
+      console.log(r.제목, "평점 :", score.trim());
+    }
+  }
 };
 
 crawler();
