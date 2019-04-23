20df2cbf618159f9cb1e78b273a3a93a8a976527
fullyalive <wogud1202@gmail.com>
2019-04-23 22:37:48 +0900

    axios-cheerio로 네이버 영화 페이지 크롤링하기

--- lecture/index.js
+++ lecture/index.js
@@ -11,17 +11,45 @@
 
 // xlsx
 
-const xlsx = require("xlsx");
-const workbook = xlsx.readFile("xlsx/data.xlsx");
+// const xlsx = require("xlsx");
+// const workbook = xlsx.readFile("xlsx/data.xlsx");
 
-const ws = workbook.Sheets.영화목록;
-const records = xlsx.utils.sheet_to_json(ws);
+// const ws = workbook.Sheets.영화목록;
+// const records = xlsx.utils.sheet_to_json(ws);
 // console.log(records);
 
-records.forEach((r, i) => {
-  console.log(i, r.제목, r.링크);
-});
+// records.forEach((r, i) => {
+//   console.log(i, r.제목, r.링크);
+// });
 
 // for (const [i, r] of records.entries()) {
 //   console.log(i, r.제목, r.링크);
 // }
+
+const xlsx = require("xlsx");
+const axios = require("axios"); // ajax 라이브러리
+const cheerio = require("cheerio"); // html 파싱
+
+const workbook = xlsx.readFile("xlsx/data.xlsx");
+const ws = workbook.Sheets.영화목록;
+const records = xlsx.utils.sheet_to_json(ws);
+
+for (const [i, r] of records.entries()) {
+  console.log(i, r.제목, r.링크);
+}
+
+const crawler = async () => {
+  await Promise.all(
+    records.map(async r => {
+      const response = await axios.get(r.링크);
+      if (response.status === 200) {
+        const html = response.data;
+        const $ = cheerio.load(html); // cheerio와 $ 를 이용해서 html tag를 가져올 수 있음
+        const score = $('.score.score_left .star_score').text();
+        console.log(r.제목, '평점 :', score.trim());
+      }
+    })
+  );
+};
+
+crawler();
