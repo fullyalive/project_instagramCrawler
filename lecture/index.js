a829bfd9c8270c866968b48d92bacfcf32e5bdd8
fullyalive <wogud1202@gmail.com>
2019-04-24 21:46:31 +0900

    csv-stringify를 이용해 크롤링 결과값을 엑셀에 출력하기

--- lecture/index.js
+++ lecture/index.js
@@ -1,4 +1,5 @@
 const parse = require("csv-parse/lib/sync");
+const stringify = require("csv-stringify/lib/sync");
 const fs = require("fs");
 const puppeteer = require("puppeteer");
 
@@ -7,6 +8,7 @@ const records = parse(csv.toString("utf-8"));
 
 const crawler = async () => {
   try {
+    const result = [];
     const browser = await puppeteer.launch({
       headless: process.env.Node_ENV === "production"
     });
@@ -21,17 +23,20 @@ const crawler = async () => {
               return tag.textContent;
             }, scoreEl);
             console.log(r[0], "평점", score.trim());
+            result[i] = [r[0], r[1], score.trim()];
           }
           await page.waitFor(3000);
           await page.close();
         } catch (e) {
-          console.log(e);
+          console.error(e);
         }
       })
     );
     await browser.close();
+    const str = stringify(result); // 2차원 배열을 문자열로 만든다.
+    fs.writeFileSync('csv/result.csv', str); 
   } catch (e) {
-    console.log(e);
+    console.error(e);
   }
 };
 
