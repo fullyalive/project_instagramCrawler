3d6e9e0f487a490abc6a2c4be45a63f97bcc0eac
fullyalive <wogud1202@gmail.com>
2019-04-24 22:08:40 +0900

    page.evaluate 사용하기

--- lecture/index.js
+++ lecture/index.js
@@ -17,13 +17,18 @@ const crawler = async () => {
         try {
           const page = await browser.newPage();
           await page.goto(r[1]);
-          const scoreEl = await page.$(".score.score_left .star_score");
-          if (scoreEl) {
-            const score = await page.evaluate(tag => {
-              return tag.textContent;
-            }, scoreEl);
-            console.log(r[0], "평점", score.trim());
-            result[i] = [r[0], r[1], score.trim()];
+          // const 태그핸들러 = await page.$(선택자);
+          const text = await page.evaluate(() => {
+            const score = document.querySelector(
+              ".score.score_left .star_score"
+            );
+            if (score) {
+              return score.textContent;
+            }
+          });
+          if (text) {
+            console.log(r[0], "평점", text.trim());
+            result[i] = [r[0], r[1], text.trim()];
           }
           await page.waitFor(3000);
           await page.close();
@@ -34,7 +39,7 @@ const crawler = async () => {
     );
     await browser.close();
     const str = stringify(result); // 2차원 배열을 문자열로 만든다.
-    fs.writeFileSync('csv/result.csv', str); 
+    fs.writeFileSync("csv/result.csv", str);
   } catch (e) {
     console.error(e);
   }
