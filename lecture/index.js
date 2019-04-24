e9c2eacede32ba28652dde020a721541fe494d96
fullyalive <wogud1202@gmail.com>
2019-04-24 21:27:22 +0900

    puppeteer 첫 크롤링

--- lecture/index.js
+++ lecture/index.js
@@ -1,17 +1,38 @@
-const parse = require('csv-parse/lib/sync');
-const fs = require('fs');
-const puppeteer = require('puppeteer');
+const parse = require("csv-parse/lib/sync");
+const fs = require("fs");
+const puppeteer = require("puppeteer");
 
-const csv = fs.readFileSync('csv/data.csv');
-const records = parse(csv.toString('utf-8'));
+const csv = fs.readFileSync("csv/data.csv");
+const records = parse(csv.toString("utf-8"));
 
 const crawler = async () => {
-  const browser = await puppeteer.launch({ headless: false});
-  const page = await browser.newPage();
-  await page.goto('https://github.com/fullyalive');
-  await page.waitFor(3000);
-  await page.close();
-  await browser.close();
-}
+  try {
+    const browser = await puppeteer.launch({
+      headless: process.env.Node_ENV === "production"
+    });
+    await Promise.all(
+      records.map(async (r, i) => {
+        try {
+          const page = await browser.newPage();
+          await page.goto(r[1]);
+          const scoreEl = await page.$(".score.score_left .star_score");
+          if (scoreEl) {
+            const score = await page.evaluate(tag => {
+              return tag.textContent;
+            }, scoreEl);
+            console.log(r[0], "평점", score.trim());
+          }
+          await page.waitFor(3000);
+          await page.close();
+        } catch (e) {
+          console.log(e);
+        }
+      })
+    );
+    await browser.close();
+  } catch (e) {
+    console.log(e);
+  }
+};
 
 crawler();
