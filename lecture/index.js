3280e187fdb7884f357f475f1b5a7ee25154a241
fullyalive <wogud1202@gmail.com>
2019-04-26 17:33:31 +0900

    이미지 크롤링 후 저장하기

--- lecture/index.js
+++ lecture/index.js
@@ -1,18 +1,25 @@
 const puppeteer = require("puppeteer"); // postman으로 요청 보냈을 때 이미지 로딩 상태 확인하기
+const fs = require("fs");
+const axios = require("axios");
+
+fs.readdir("imgs_unsplash", err => {
+  if (err) {
+    console.error("imgs_unsplash 폴더가 없어 imgs_unsplash 폴더를 생성합니다.");
+    fs.mkdirSync("imgs_unsplash");
+  }
+});
 
 const crawler = async () => {
   try {
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
-    const crawlingNum = 50;
-    const crawlingTag = "figure";
     await page.goto("https://unsplash.com");
     let result = [];
-    while (result.length <= crawlingNum) {
+    while (result.length <= 50) {
       const srcs = await page.evaluate(() => {
         window.scrollTo(0, 0);
         let imgs = [];
-        const imgEls = document.querySelectorAll(crawlingTag); // 사이트 바뀌었을 때 클래스 적절히 바꾸기
+        const imgEls = document.querySelectorAll("figure"); // 사이트 바뀌었을 때 클래스 적절히 바꾸기
         if (imgEls.length) {
           // querySelectorAll은 map이 아니라 forEach를 상용한다.
           imgEls.forEach(v => {
@@ -30,9 +37,19 @@ const crawler = async () => {
         return imgs;
       });
       result = result.concat(srcs);
-      await page.waitForSelector(crawlingTag);
+      await page.waitForSelector("figure");
+      console.log("새 이미지 태그 로딩 완료!");
     }
     console.log(result);
+    result.forEach(async src => {
+      const imgResult = await axios.get(src.replace(/\?.*$/, ""), {
+        responseType: "arraybuffer"
+      });
+      fs.writeFileSync(
+        `imgs_unsplash/${new Date().valueOf()}.jpeg`,
+        imgResult.data
+      );
+    });
     await page.close();
     await browser.close();
   } catch (e) {
